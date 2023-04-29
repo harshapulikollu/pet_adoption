df32233902696af32e1de4d685f634719dd5b8df
harsha pulikollu <harshapulikollu@outlook.com>
2023-04-29 21:29:54 +0530

    Added credits and source code links Cleaned code, moved into separate widgets

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:neopop/neopop.dart';
-import 'package:pet_adoption_hn/utils/colors.dart';
 
+import '../widgets/Credits_text.dart';
 import '../widgets/custom_text.dart';
+import '../widgets/theme_switcher_button.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -13,47 +11,38 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: PreferredSize(preferredSize: Size(MediaQuery.of(context).size.width, kToolbarHeight),
-        child: Row(
-          children: [
-            IconButton(onPressed: (){
-              Navigator.pop(context);
-            }, icon: const Icon(Icons.navigate_before)),
-            CustomText('Settings', style: Theme.of(context).textTheme.headlineLarge,)
-          ],
-        ),),
+        appBar: PreferredSize(
+          preferredSize:
+              Size(MediaQuery.of(context).size.width, kToolbarHeight),
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.navigate_before)),
+              CustomText(
+                'Settings',
+                style: Theme.of(context).textTheme.headlineLarge,
+              )
+            ],
+          ),
+        ),
         body: Column(
           children: [
             ListTile(
-              title: CustomText('App theme',style: Theme.of(context).textTheme.titleLarge,),
-              subtitle:
-              CustomText('Switch app theme by selecting your preferred theme mode', style: Theme.of(context).textTheme.titleMedium,),
-              trailing: ValueListenableBuilder(
-                  valueListenable: Hive.box('app_settings').listenable(),
-                builder: (context, box, child) {
-                  final isDarkMode = box.get('isDarkModeEnabled', defaultValue: true);
-                  return NeoPopButton(
-                    color: isDarkMode ?  kSecondaryButtonLightColor : kPrimaryButtonColor,
-                    bottomShadowColor: isDarkMode ? kShadowColorDarkGreen : kShadowColorGrey,
-                    rightShadowColor: isDarkMode ? kShadowColorGreen : kShadowColorGrey,
-                    border: Border.all(
-                      color: isDarkMode ? kBorderColorGreen : kShadowColorGrey,
-                      width: kButtonBorderWidth,
-                    ),
-                    onTapUp: () => HapticFeedback.lightImpact(),
-                    onTapDown: (){
-                      HapticFeedback.lightImpact();
-                      box.put('isDarkModeEnabled', !isDarkMode);
-                    },
-                    child: Padding(
-                      padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                      child: CustomText(isDarkMode ? 'Light' : 'Dark'),
-                    ),
-                  );
-                }
+              title: CustomText(
+                'App theme',
+                style: Theme.of(context).textTheme.titleLarge,
               ),
-            )
+              subtitle: CustomText(
+                'Switch app theme by selecting your preferred theme mode',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              trailing: const ThemeSwitcherButton(),
+            ),
+            const SizedBox(height: 48.0,),
+            const CreditsText(),
           ],
         ),
       ),
---
+++ lib/widgets/theme_switcher_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:neopop/widgets/buttons/neopop_button/neopop_button.dart';
+
+import '../utils/colors.dart';
+import 'custom_text.dart';
+
+class ThemeSwitcherButton extends StatelessWidget {
+  const ThemeSwitcherButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: Hive.box('app_settings').listenable(),
+        builder: (context, box, child) {
+          final isDarkMode = box.get('isDarkModeEnabled', defaultValue: true);
+          return NeoPopButton(
+            color: isDarkMode ?  kSecondaryButtonLightColor : kPrimaryButtonColor,
+            bottomShadowColor: isDarkMode ? kShadowColorDarkGreen : kShadowColorGrey,
+            rightShadowColor: isDarkMode ? kShadowColorGreen : kShadowColorGrey,
+            border: Border.all(
+              color: isDarkMode ? kBorderColorGreen : kShadowColorGrey,
+              width: kButtonBorderWidth,
+            ),
+            onTapUp: () => HapticFeedback.lightImpact(),
+            onTapDown: (){
+              HapticFeedback.lightImpact();
+              box.put('isDarkModeEnabled', !isDarkMode);
+            },
+            child: Padding(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              child: CustomText(isDarkMode ? 'Light' : 'Dark'),
+            ),
+          );
+        }
+    );
+  }
+}
