1a113132b31ce95764e38622c77b24d91808231c
harsha pulikollu <harshapulikollu@outlook.com>
2023-04-30 20:38:46 +0530

    cleaned code in widget tests

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:pet_adoption_hn/cubit/home_cubit.dart';
 import 'package:pet_adoption_hn/model/pet.dart';
